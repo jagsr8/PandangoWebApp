58bce0d13fa4f96360edd7fdc42a3bd5dca80606
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2016-01-28 10:12:09 -0500

    Changed the routes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  res.send('get Request made');
   console.log('Get request made');
 });
 
