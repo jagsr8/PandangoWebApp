ff7fd3035ca711940e19eae06c2d547bd6981fb0
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2016-02-08 11:48:01 -0500

    Added body parser

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
-
+var app = express();
+app.use(express.bodyParser());
 /* GET home page. */
 router.get('/', function(req, res) {
   //res.render('index', { title: 'Express' });
-  res.json(req.body);
+  res.send(req.body);
 });
 
 module.exports = router;
