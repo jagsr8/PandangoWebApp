99029f3e46da07b8b26aabd09b8809266885992f
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2016-02-08 10:58:08 -0500

    Added a specific body variable

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   //res.render('index', { title: 'Express' });
-  res.send(req.body);
+  res.send(req.body.name);
 });
 
 module.exports = router;
