3d492b33e08a34e25d43b82c4d7951db720dfe39
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2016-02-08 10:51:57 -0500

    Added a send method in one route

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('User.db');
 
-db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='Users'",
-       function(err, rows) {
-  if(err !== null) {
-    console.log(err);
-  }
-  else if(rows === undefined) {
-    db.run("CREATE TABLE Users (_id INTEGER, user_name TEXT, password TEXT)", function(err) {
-      if(err !== null) {
-        console.log(err);
-      }
-      else {
-        console.log("SQL Table 'bookmarks' initialized.");
-      }
-    });
-  }
-  else {
-    console.log("SQL Table 'bookmarks' already initialized.");
-  }
-});
 
 /* GET home page. */
-router.get('/:id', function(req, res, next) {
+router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
-  res.json(req.body);
+  res.send(req.body);
 });
 
 module.exports = router;
