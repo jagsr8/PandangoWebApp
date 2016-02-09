533b63c81ef9cf42fb76b74053eddacaffaa82c0
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2016-02-08 22:27:06 -0500

    Added sql table and a get login status

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,41 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database('User.db');
+
+var username = "";
+var password = "";
+
+db.serialize(function() {
+  console.log('creating table');
+  db.run("CREATE TABLE if not exists Users (_id INTEGER, username TEXT, password TEXT)");
+  console.log('table created');
+  //var entry1 = db.prepare();
+  //var entry2 = db.prepare("INSERT INTO Users (_id, username, password) VALUES (0, 'jagsr', 'password');");
+  db.run("INSERT INTO Users VALUES(?, ?, ?)", (0, 'ganhari123', 'password'));
+});
+
+
 /* GET home page. */
 router.get('/:username/:password', function(req, res) {
   //res.render('index', { title: 'Express' });
-  res.send(req.params.username + " " + req.params.password);
+  username = req.params.username;
+  password = req.params.password;
+  var sqlstat = "SELECT * FROM Users WHERE username = '";
+  var sqlstat = sqlstat.concat(username, "';");
+  db.each(sqlstat, function(err, row) {
+  	if (err) {
+  		res.send('User does not exist');
+  	} else {
+  		if (row.password === password) {
+  			res.send('0');
+  		} else {
+  			res.send('Password does not match!');
+  		}
+  	}
+  });
 });
 
+db.close();
 module.exports = router;
