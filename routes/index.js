a6331751ed19c6efff283c2b0d0d40c288f92139
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2016-02-14 23:30:18 -0500

    Added stuff to package.json and added the index.js routes code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,91 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var mysql = require("mysql");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var lastRegisteredID = 2;
+
+
+// First you need to create a connection to the db
+var con = mysql.createConnection({
+  host: "us-cdbr-iron-east-03.cleardb.net",
+  user: "b8c1a58913c797",
+  password: "05ec03a0",
+  database: "heroku_d5011e791776e3d"
 });
 
+
+
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+setInterval(function () {
+    con.query('SELECT 1');
+}, 5000);
+
+router.get('/:username/:password', function(req, res){
+  var username = req.params.username;
+  var password = req.params.password;
+  con.query('SELECT * FROM users WHERE username = ?', username, function(err, rows){
+    if (err) {
+      res.send('0');
+    } else {
+      if (rows[0].password === password) {
+        res.send(username);
+      } else {
+        res.send('0');
+      }
+    }
+  });
+});
+
+router.post('/userRegistration', function(req, response){
+  console.log(req.body.username);
+  con.query('SELECT * FROM users WHERE username = ?', req.body.username, function(err, rows){
+      if (err) {
+           console.log(err); 
+      } else {
+        if (rows.length === 0) {
+          var newUser = {id: lastRegisteredID, name: req.body.name, username: req.body.username, password: req.body.password};
+            con.query('INSERT INTO users SET ?', newUser, function(err, res){
+                if (err) {
+                  console.log(err);
+                } else {
+                  lastRegisteredID = lastRegisteredID + 1;
+                  response.send('user added sucessfully!');
+                }
+
+            });
+          } else {
+            response.send('User already added');
+          }
+      }
+  });
+});
+
+router.post('/editProfile/:username/:password', function(req, res){
+  var username = req.params.username;
+  var password = req.params.password;
+  con.query('UPDATE users SET password = ? Where username = ?', [password, username],function (err, result) {
+    if (err){
+      console.log(err); 
+      throw err;
+    } else {
+      console.log('Changed password!!');
+      res.send('Changed Password Sucessfully!');
+    }
+  }
+);
+});
+
+/*con.end(function(err) {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+});*/
 module.exports = router;
