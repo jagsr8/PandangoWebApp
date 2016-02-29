e74bba00fd8591d77816c85e3dae947ddd709468
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2016-02-29 12:45:29 -0500

    Added a debug statement to the routes

--- routes/index.js
+++ routes/index.js
@@ -77,7 +77,7 @@ router.get('/getUserProfile/:username', function(req, res) {
   var username = req.params.username;
   con.query('SELECT * FROM users WHERE username = ?', username, function(err, rows){
     if (err) {
-      console.log(err);
+      console.log("IN HERE");
       res.send('0');
     } else {
       res.send(rows);
