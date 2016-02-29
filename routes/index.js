be231f87b058b5e6ad5f994371bd21536147c2c1
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2016-02-29 14:18:25 -0500

    Sending the request back

--- routes/index.js
+++ routes/index.js
@@ -73,9 +73,8 @@ router.get('/:username/:password', function(req, res){
   });
 });
 
-router.get('/userProf/:username', function(req, res) {
-  var username = req.params.username;
-  res.send(username);
+router.get('/userProf/:un', function(req, res) {
+  res.send(req.params.un);
   /*con.query('SELECT * FROM users WHERE username = ?', username, function(err, rows){
     if (err) {
       res.send('HELLO I AM ERRORING');
